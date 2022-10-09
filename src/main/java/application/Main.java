7561a078602df68a0e428370b7ae9c173bc925fa
Laqi17 <romanrayzanov@gmail.com>
2022-10-09 21:35:59 +0300

    first commit

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,16 @@
+package application;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8080), 0);
+        server.start();
+
+        //TODO: create handle method to handle simple POST and GET requests
+    }
+
+}
